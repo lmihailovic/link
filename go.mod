37e0e1cf3278b11c9cd928d6782abeb993c3f3a1
lmihailovic <miha.luka1006@gmail.com>
2025-08-09 00:35:41 +0200

    first naive implementation

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module link-parser
 
 go 1.24
+
+require golang.org/x/net v0.43.0
--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"golang.org/x/net/html"
+	"os"
+)
+
 func main() {
+	filePath := os.Args[1]
+
+	fileBytes, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	links := make(map[string]string)
+
+	doc, err := html.Parse(fileBytes)
+	if err != nil {
+		panic(err)
+	}
+	for n := range doc.Descendants() {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			links[n.Attr[0].Val] = n.FirstChild.Data
+		}
+	}
+
+	for k, v := range links {
+		println(k, v)
+	}
 
 }
