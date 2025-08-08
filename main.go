2c6d4c7d5934169487e7f49ee3c97e4c15ec71f4
lmihailovic <miha.luka1006@gmail.com>
2025-08-09 01:46:58 +0200

    extracted parsing logic into function

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"golang.org/x/net/html"
 	"os"
+	"strings"
 )
+
+func parseLinks(doc *html.Node) map[string]string {
+	links := make(map[string]string)
+
+	for n := range doc.Descendants() {
+		var nodeText string
+		if n.Type == html.ElementNode && n.Data == "a" {
+			nodeText = strings.TrimSpace(n.FirstChild.Data)
+			links[n.Attr[0].Val] = nodeText
+		}
+	}
+	return links
+}
 
 func main() {
 	filePath := os.Args[1]
@@ -13,20 +28,15 @@ func main() {
 		panic(err)
 	}
 
-	links := make(map[string]string)
-
 	doc, err := html.Parse(fileBytes)
 	if err != nil {
 		panic(err)
 	}
-	for n := range doc.Descendants() {
-		if n.Type == html.ElementNode && n.Data == "a" {
-			links[n.Attr[0].Val] = n.FirstChild.Data
-		}
-	}
+
+	links := parseLinks(doc)
 
 	for k, v := range links {
-		println(k, v)
+		fmt.Printf("\npath: %v\ntext: %v\n", k, v)
 	}
 
 }
