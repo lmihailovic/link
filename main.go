e3ea9d45f806faf1c6536240b85636effe4559ba
lmihailovic <miha.luka1006@gmail.com>
2025-08-09 02:15:15 +0200

    refactored code

--- main.go
+++ main.go
@@ -7,6 +7,19 @@ import (
 	"strings"
 )
 
+func parseLinkChildrenText(p *html.Node) string {
+	var nodeText string
+
+	for c := p.FirstChild; c != nil; c = c.NextSibling {
+		if c.Type == html.TextNode {
+			nodeText += c.Data
+		} else {
+			nodeText += parseLinkChildrenText(c)
+		}
+	}
+	return nodeText
+}
+
 func ParseLinks(doc *html.Node) map[string]string {
 	links := make(map[string]string)
 
@@ -20,29 +33,15 @@ func ParseLinks(doc *html.Node) map[string]string {
 	return links
 }
 
-func parseLinkChildrenText(p *html.Node) string {
-	var nodeText string
-
-	for c := p.FirstChild; c != nil; c = c.NextSibling {
-		if c.Type == html.TextNode {
-			nodeText += c.Data
-		} else {
-			nodeText += parseLinkChildrenText(c)
-		}
-	}
-
-	return nodeText
-}
-
 func main() {
-	filePath := os.Args[1]
+	htmlPath := os.Args[1]
 
-	fileBytes, err := os.Open(filePath)
+	htmlBytes, err := os.Open(htmlPath)
 	if err != nil {
 		panic(err)
 	}
 
-	doc, err := html.Parse(fileBytes)
+	doc, err := html.Parse(htmlBytes)
 	if err != nil {
 		panic(err)
 	}
@@ -52,5 +51,4 @@ func main() {
 	for k, v := range links {
 		fmt.Printf("\npath: %v\ntext: %v\n", k, v)
 	}
-
 }
