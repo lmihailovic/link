a8ad267311dbab2ddb976c0102c6acb9d8e54d33
lmihailovic <miha.luka1006@gmail.com>
2025-08-09 02:08:47 +0200

    recursive traversal of link subelements

--- main.go
+++ main.go
@@ -7,26 +7,31 @@ import (
 	"strings"
 )
 
-func parseLinks(doc *html.Node) map[string]string {
+func ParseLinks(doc *html.Node) map[string]string {
 	links := make(map[string]string)
 
 	for n := range doc.Descendants() {
 		var nodeText string
 		if n.Type == html.ElementNode && n.Data == "a" {
-
-			for c := n.FirstChild; c != nil; c = c.NextSibling {
-				if c.Type == html.TextNode {
-					nodeText += strings.TrimSpace(c.Data)
-				} else if c.Type == html.ElementNode && c.FirstChild != nil {
-					nodeText += strings.TrimSpace(c.FirstChild.Data)
-				}
-			}
-
-			//nodeText += strings.TrimSpace(n.FirstChild.Data)
-			links[n.Attr[0].Val] = nodeText
+			nodeText = parseLinkChildrenText(n)
+			links[n.Attr[0].Val] = strings.TrimSpace(nodeText)
 		}
 	}
 	return links
+}
+
+func parseLinkChildrenText(p *html.Node) string {
+	var nodeText string
+
+	for c := p.FirstChild; c != nil; c = c.NextSibling {
+		if c.Type == html.TextNode {
+			nodeText += c.Data
+		} else {
+			nodeText += parseLinkChildrenText(c)
+		}
+	}
+
+	return nodeText
 }
 
 func main() {
@@ -42,7 +47,7 @@ func main() {
 		panic(err)
 	}
 
-	links := parseLinks(doc)
+	links := ParseLinks(doc)
 
 	for k, v := range links {
 		fmt.Printf("\npath: %v\ntext: %v\n", k, v)
